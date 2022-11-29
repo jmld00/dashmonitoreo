bd24db0898b8c9ce5603a68891dff498c4e6adf3
jmld00 <118228098+jmld00@users.noreply.github.com>
2022-11-29 17:34:56 -0600

    Update script.py

--- script.py
+++ script.py
@@ -143,6 +143,7 @@ sidebar = html.Div([
         options=[{'label': i, 'value': i} for i in sets],
         value = 'Ambas',
         style={'width': '100%'},
+        maxHeight= 75,
         clearable=False
     )], style={'padding': '0.5rem 0.5rem 0.5rem 0.5rem'}),
     
